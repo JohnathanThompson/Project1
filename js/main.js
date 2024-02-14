ef284b55352ea0fb0a6f0a7a175ec988a7d43908
Johnathan Thompson <Thomp5je@mail.uc.edu>
2024-02-14 01:08:48 -0500

    National Health Data Choropleth

--- js/main.js
+++ js/main.js
@@ -4,18 +4,18 @@
 
 Promise.all([
   d3.json('data/counties-10m.json'),
-  d3.csv('data/population.csv')
+  d3.csv('data/national_health_data.csv')
 ]).then(data => {
   const geoData = data[0];
   const countyPopulationData = data[1];
+  const nationalHealthData = data[2];
 
   // Combine both datasets by adding the population density to the TopoJSON file
-  console.log(geoData);
   geoData.objects.counties.geometries.forEach(d => {
     console.log(d);  
-    for (let i = 0; i < countyPopulationData.length; i++) {
-      if (d.id === countyPopulationData[i].cnty_fips) {
-        d.properties.pop = +countyPopulationData[i].Value;
+    for (let i = 0; i < nationalHealthData.length; i++) {
+      if (d.id === nationalHealthData[i].cnty_fips) {
+        d.properties.pop = +nationalHealthData[i].poverty_perc;
       }
 
     }
