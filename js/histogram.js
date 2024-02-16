05878ef288c428bd9973927a15fb6fe66d53c81d
Johnathan Thompson <Thomp5je@mail.uc.edu>
2024-02-16 13:41:04 -0500

    Progress made os far

---
+++ js/histogram.js
@@ -0,0 +1,83 @@
+class Histogram {
+
+    /**
+     * Class constructor with basic configuration
+     * @param {Object}
+     * @param {Array}
+     */
+    constructor(_config, _data, _title, _units, _acronym) {
+      this.config = {
+        parentElement: _config.parentElement,
+        containerWidth: _config.containerWidth || 1000,
+        containerHeight: _config.containerHeight || 500,
+        margin: _config.margin || {top: 10, right: 10, bottom: 10, left: 10},
+        tooltipPadding: 10,
+        legendBottom: 50,
+        legendLeft: 50,
+        legendRectHeight: 12, 
+        legendRectWidth: 150
+      }
+      this.data = _data;
+      // this.config = _config;
+  
+      this.us = _data;
+  
+      this.title = _title
+  
+      this.units = _units
+  
+      this.acronym = _acronym
+  
+      this.active = d3.select(null);
+  
+      this.initVis();
+    }
+    
+    /**
+     * We initialize scales/axes and append static elements, such as axis titles.
+     */
+    initVis() {
+      let vis = this;
+  
+      // Calculate inner chart size. Margin specifies the space around the actual chart.
+      vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+      vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+  
+      // Define size of SVG drawing area
+      vis.svg = d3.select(vis.config.parentElement).append('svg')
+          .attr('class', 'center-container')
+          .attr('width', vis.config.containerWidth)
+          .attr('height', vis.config.containerHeight);
+  
+        
+      vis.svg.append('rect')
+              .attr('class', 'background center-container')
+              .attr('height', vis.config.containerWidth ) //height + margin.top + margin.bottom)
+              .attr('width', vis.config.containerHeight + 100) //width + margin.left + margin.right)
+              .on('click', vis.clicked);
+  
+    
+      vis.projection = d3.geoAlbersUsa()
+              .translate([vis.width /2 , vis.height / 2])
+              .scale(vis.width);
+  
+      vis.colorScale = d3.scaleLinear()
+        .domain(d3.extent(vis.data.objects.counties.geometries, d => d.properties[this.acronym]))
+          .range(['#cfe2f2', '#0d306b'])
+          .interpolate(d3.interpolateHcl);
+  
+      vis.path = d3.geoPath()
+              .projection(vis.projection);
+  
+      vis.g = vis.svg.append("g")
+              .attr('class', 'center-container center-items us-state')
+              .attr('transform', 'translate('+vis.config.margin.left+','+vis.config.margin.top+')')
+              .attr('width', vis.width + vis.config.margin.left + vis.config.margin.right)
+              .attr('height', vis.height + vis.config.margin.top + vis.config.margin.bottom)
+  
+
+  
+    }
+  
+    
+  }
--- js/main.js
+++ js/main.js
@@ -16,6 +16,19 @@ Promise.all([
         d.properties["pov"] = +nationalHealthData[i].poverty_perc;
         d.properties["med"] = +nationalHealthData[i].median_household_income
         d.properties["edu"] = +nationalHealthData[i].education_less_than_high_school_percent;
+        d.properties["air"] = +nationalHealthData[i].air_quality;
+        d.properties["park"] = +nationalHealthData[i].park_access;
+        d.properties["inactive"] = +nationalHealthData[i].percent_inactive;
+        d.properties["smoke"] = +nationalHealthData[i].percent_smoking;
+        d.properties["urban"] = +nationalHealthData[i].urban_rural_status;
+        d.properties["elder"] = +nationalHealthData[i].elderly_percentage;
+        d.properties["hosp"] = +nationalHealthData[i].number_of_hospitals;
+        d.properties["phys"] = +nationalHealthData[i].number_of_primary_care_physicians;
+        d.properties["ins"] = +nationalHealthData[i].percent_no_heath_insurance;
+        d.properties["hbp"] = +nationalHealthData[i].percent_high_blood_pressure;
+        d.properties["chd"] = +nationalHealthData[i].percent_coronary_heart_disease;
+        d.properties["stroke"] = +nationalHealthData[i].percent_stroke;
+        d.properties["chol"] = +nationalHealthData[i].percent_high_cholesterol
       }
 
 
